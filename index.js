d37da503d44e4e817c3e6354da6f6bf719e55596
ariful-islam <arifulislam204909@gmail.com>
2023-05-16 00:53:38 +0600

    crud operations added

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ async function run() {
     await client.connect();
 
     const serviceCollection = client.db("carDoctor").collection("services");
+    const bookingCollection = client.db("carDoctor").collection("bookings");
 
     app.get("/services", async (req, res) => {
       const cursor = serviceCollection.find();
@@ -36,12 +37,42 @@ async function run() {
 
     app.get("/services/:id", async (req, res) => {
       const id = req.params.id;
+      console.log(id);
       const query = { _id: new ObjectId(id) };
-      const result = await serviceCollection.findOne(query);
-      console.log(result);
 
+      const options = {
+        // Include only the `title` and `imdb` fields in the returned document
+        projection: { title: 1, service_id: 1, price: 1 , img: 1},
+      };
+
+      const result = await serviceCollection.findOne(query, options);
+      console.log(result);
       res.send(result);
     });
+
+    //bookings
+
+    app.get('/bookings', async(req, res)=>{
+      let query ={};
+      if(req?.query?.email){
+        query ={email: req.query.email}
+      }
+      const result = await bookingCollection.find(query).toArray();
+      res.send(result)
+    })
+
+    app.post('/bookings', async(req, res)=>{
+
+      const booking =req.body;
+      console.log(booking);
+      const result =await bookingCollection.insertOne(booking);
+      res.send(result)
+
+    })
+
+
+
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
