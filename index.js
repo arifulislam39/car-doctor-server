c7aab18f7885550a61e7c5e150905115bb44b6d7
ariful-islam <arifulislam204909@gmail.com>
2023-05-17 00:18:58 +0600

    finish

--- index.js
+++ index.js
@@ -22,6 +22,24 @@ const client = new MongoClient(uri, {
   },
 });
 
+const verifyJWT = (req, res, next) => {
+  console.log("hitting verify JWT");
+  console.log(req.headers.authorization);
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: "unauthorized" });
+  }
+  const token = authorization.split(" ")[1];
+  console.log("token inside verify jwt", token);
+  jwt.verify(token, process.env.ACCESS_TOKEN, (error, decoded) => {
+    if (error) {
+      return res.status(403).send({ error: true, message: "unauthorized" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -38,7 +56,7 @@ async function run() {
       const token = jwt.sign(user, process.env.ACCESS_TOKEN, {
         expiresIn: "1h",
       });
-      res.send({token})
+      res.send({ token });
     });
 
     //services
@@ -65,7 +83,14 @@ async function run() {
 
     //bookings
 
-    app.get("/bookings", async (req, res) => {
+    app.get("/bookings", verifyJWT, async (req, res) => {
+      const decoded=req.decoded;
+      console.log("came back after verify", decoded);
+      if(decoded.email !==req.query.email){
+        return res.status(403).send({error: 1, message: "forbidden access"})
+      }
+
+
       let query = {};
       if (req?.query?.email) {
         query = { email: req.query.email };
